d99866de394f9a78daee3690a979777f1e35d24c
ridget <tom.ridge@tworedkites.com>
2014-06-30 20:37:57 +1000

    log transitions

--- app/app.js
+++ app/app.js
@@ -6,7 +6,9 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'transactions', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver,
+  LOG_TRANSITIONS: true,
+  LOG_TRANSITIONS_INTERNAL: true
 });
 
 loadInitializers(App, 'transactions');
