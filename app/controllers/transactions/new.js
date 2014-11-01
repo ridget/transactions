13a5210dca4ddf3051610571403ffa1b823706b1
Tom Ridge <tom.ridge@tworedkites.com>
2014-11-01 13:37:24 +1000

    remove all the things

--- app/controllers/transactions/new.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-
-// Make TransactionsNewController an ObjectController, 
-// so that it will proxy properties. !important
-export default Ember.ObjectController.extend({
-
-});
