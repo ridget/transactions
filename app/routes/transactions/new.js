98f604f82c3820db958120b4ee2f3462f0cb468a
Tom Ridge <tom.ridge@tworedkites.com>
2014-11-01 10:07:02 +1000

    first tests.. YAY

--- app/routes/transactions/new.js
+++ app/routes/transactions/new.js
@@ -1,16 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function(){
-        return this.store.createRecord("transaction", { amount: 0, name: 'New Transaction' });
-    },
-
     actions: {
       createTransaction: function(){
         var self = this;
         var transaction = this.controller.get('model');
-        // TODO: set date format the way you want it.
-        transaction.set('occurredOn', new Date);
+        transaction.set('occurredOn', new Date());
         transaction.save().then(function(){
           self.transitionTo('transactions.index');
         });
